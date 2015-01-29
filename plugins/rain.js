793937d3cb62a90ee727caf9c4f942f3dbaf835e
Yarek Tyshchenko <yarek.tyshchenko@affiliatewindow.com>
2015-01-29 15:21:14 +0000

    Add location to !rain command

--- plugins/rain.js
+++ plugins/rain.js
@@ -1,5 +1,6 @@
 var _ = require('underscore');
 var https = require('https');
+var geocoder = require('geocoder');
 
 module.exports = function() {
     this.name = 'Rain';
@@ -9,8 +10,8 @@ module.exports = function() {
     var token = this.noddy.getConfig().plugins.rain.token;
     var location = this.noddy.getConfig().plugins.rain.location;
 
-    var getData = function(callback) {
-        https.get('https://api.forecast.io/forecast/'+token+'/'+location, function(res) {
+    var getData = function(loc, callback) {
+        https.get('https://api.forecast.io/forecast/'+token+'/'+loc, function(res) {
             var response = '';
             res.on('data', function(chunk) {
                 response += chunk;
@@ -23,48 +24,70 @@ module.exports = function() {
 
     var say = this.noddy.say;
 
+    var processRainDataForLocation = function(loc, to) {
+        getData(loc, function(json) {
+            if (! json.minutely) {
+                // No minutely data available
+                // Lets try hourly
+                say(to, json.currently.summary +", "+ json.hourly.summary);
+                return;
+            }
+            var graph = '';
+            var cache = 0.0;
+            var sampled = json.minutely.data.map(function(slice) {
+                return slice.precipProbability;
+            })
+            .reduce(function(list, curr, index) {
+                var gamma = 3;
+                if (index % gamma == gamma-1) {
+                    list[Math.floor(index / gamma)] = (cache+curr) / gamma;
+                    cache = 0.0;
+                } else {
+                    cache += curr;
+                }
+                return list;
+            }, []);
+            //console.log(sampled);
+            _.forEach(sampled, function(slice) {
+                graph += function() {
+                    var g = Math.floor(slice * 9);
+                    //console.log(g);
+                    switch (g) {
+                        case 0: return '\u2001';
+                        case 1: return '\u2581';
+                        case 2: return '\u2582';
+                        case 3: return '\u2583';
+                        case 4: return '\u2584';
+                        case 5: return '\u2585';
+                        case 6: return '\u2586';
+                        case 7: return '\u2587';
+                        case 8: return '\u2588';
+                        case 9: return '\u2589';
+                        default: return '.';
+                    }
+                }();
+            });
+            say(to, json.minutely.summary + " |"+graph+"|");
+        });
+    }
+
     this.commands = {
         rain: function(from, to) {
-            // Is it about to rain?
-            getData(function(json) {
-                //console.log(json.minutely.data);
-                var graph = '';
-                var cache = 0.0;
-                var sampled = json.minutely.data.map(function(slice) {
-                    return slice.precipProbability;
-                })
-                .reduce(function(list, curr, index) {
-                    var gamma = 3;
-                    if (index % gamma == gamma-1) {
-                        list[Math.floor(index / gamma)] = (cache+curr) / gamma;
-                        cache = 0.0;
-                    } else {
-                        cache += curr;
-                    }
-                    return list;
-                }, []);
-                //console.log(sampled);
-                _.forEach(sampled, function(slice) {
-                    graph += function() {
-                        var g = Math.floor(slice * 9);
-                        //console.log(g);
-                        switch (g) {
-                            case 0: return '\u2001';
-                            case 1: return '\u2581';
-                            case 2: return '\u2582';
-                            case 3: return '\u2583';
-                            case 4: return '\u2584';
-                            case 5: return '\u2585';
-                            case 6: return '\u2586';
-                            case 7: return '\u2587';
-                            case 8: return '\u2588';
-                            case 9: return '\u2589';
-                            default: return '.';
-                        }
-                    }();
+            // [location] Is it about to rain?
+            var text = this.getText(arguments, 2);
+            if (text) {
+                geocoder.geocode(text, function (err, data) {
+                    if (err) return;
+                    var l = data.results[0].geometry.location;
+                    var locationString = [l.lat, l.lng].join(',');
+                    //console.log(data.results[0].geometry);
+                    console.log(locationString);
+                    // do stuff with data
+                    processRainDataForLocation(locationString, to);
                 });
-                say(to, json.minutely.summary + " |"+graph+"|");
-            });
+            } else {
+                processRainDataForLocation(location, to);
+            }
         }
     }
 }
