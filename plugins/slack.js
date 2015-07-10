dc53d738092650a69888a88b7802a039f92309e4
Yarek Tyshchenko <yarek.tyshchenko@affiliatewindow.com>
2015-07-10 11:01:25 +0100

    Fixed up slack relayer plugin

--- plugins/slack.js
+++ plugins/slack.js
@@ -3,58 +3,59 @@ var Slack = require('slack-client');
 module.exports = function() {
     this.name = 'Slack';
     var token = this.getConfigVar("token");
-
-    var slack = new Slack(token, true, true);
-    var sendMessage = function(){};
-    slack.on('open', function () {
-        //console.log(slack);
-
-        var channels = Object.keys(slack.channels)
-            .map(function (k) { return slack.channels[k]; })
-            .filter(function (c) { return c.is_member; })
-            .map(function (c) { return c.name; });
-     
-     
-        console.log('Welcome to Slack. You are ' + slack.self.name + ' of ' + slack.team.name);
-     
-        if (channels.length > 0) {
-            console.log('You are in: ' + channels.join(', '));
-        }
-        else {
-            console.log('You are not in any channels.');
-        }
-    });
-
-    slack.on('error', function(e) {
-        console.log(e);
-        console.log(e.stack);
-    });
-
+    var relayChannel = "test";
     var say = this.noddy.say;
 
-    slack.on('message', function(message) {
+    var slack;
+    var sendMessage = function(from, message) {
+        if (!slack) return;
+        if (!slack.connected) return;
+        var c = slack.getChannelByName(relayChannel);
+        c.postMessage({text: message, username:from});
+    }
+
+    var onOpen = function(){
+        console.log('Welcome to Slack. You are ' + slack.self.name + ' of ' + slack.team.name);
+    };
+
+    var onMessage = function(message) {
         var channel = slack.getChannelGroupOrDMByID(message.channel);
         var user = slack.getUserByID(message.user);
-        if (message.type === 'message' && channel.name == "gold") {
+        if (!user) return; // Don't trigger on non-existing users
+        if (message.subtype === 'bot_message') return; // Don't trigger on bot messages
+
+        if (message.type === 'message' && channel.name == relayChannel) {
             var trimmedMessage = message.text.trim();
-            say('#gold', user.name + ': ' + trimmedMessage);
+            say('#' + relayChannel, user.name + ': ' + trimmedMessage);
         }
-    });
+    };
+
+    var onError = function(e) {
+        console.log(e);
+        console.log(e.stack);
+    }
 
     this.init = function(payload) {
-        slack.login();
-        sendMessage = function(from, message) {
-            var c = slack.getChannelByName("gold");
-            //console.log(c);
-            //c.send(message);
-            c.postMessage({text: message, username:from});
+        if (payload) {
+            slack = payload.slack;
+        } else {
+            slack = new Slack(token, true, true);
+            slack.on('open', onOpen);
+            slack.on('error', onError);
+            slack.on('message', onMessage);
+            slack.login();
+        }
+    }
+
+    this.destroy = function() {
+        return {
+            slack: slack
         }
     }
      
     this.events = {
         message: function(from, channel, message) {
-            if (channel.toLowerCase() == "#gold") {
-                console.log(from, channel, message);
+            if (channel.toLowerCase() == "#" + relayChannel) {
                 sendMessage(from, message);
             }
         }
